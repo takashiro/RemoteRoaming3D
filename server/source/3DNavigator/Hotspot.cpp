d272aecfefb36b98e1fd7db374812d0ff6113086
Takashiro <takashiro@qq.com>
2014-03-20 22:12:04 +0800

    add image, audio & video list into Hotspots
    Signed-off-by: Takashiro <takashiro@qq.com>

--- server/source/3DNavigator/Hotspot.cpp
+++ server/source/3DNavigator/Hotspot.cpp
@@ -23,6 +23,22 @@ Hotspot::Hotspot(const Json::Value &value)
 	setName(value[0].asString());
 	setPosition(core::vector3df(value[1][0].asFloat(), value[1][1].asFloat(), value[1][2].asFloat()));
 	setSize(core::dimension2d<f32>(value[2][0].asFloat(), value[2][1].asFloat()));
+	setDescription(value[3].asString());
+
+	for(Json::Value::iterator i = value[4].begin(); i != value[4].end(); i++)
+	{
+		addImage((*i).asString());
+	}
+
+	for(Json::Value::iterator i = value[5].begin(); i != value[5].end(); i++)
+	{
+		addAudio((*i).asString());
+	}
+
+	for(Json::Value::iterator i = value[6].begin(); i != value[6].end(); i++)
+	{
+		addVideo((*i).asString());
+	}
 }
 
 std::string Hotspot::toJson() const
@@ -30,10 +46,36 @@ std::string Hotspot::toJson() const
 	Json::Value value;
 	Json::FastWriter writer;
 
-	value[0][0] = _pos.X;
-	value[0][1] = _pos.Y;
-	value[0][2] = _pos.Z;
-	value[1] = _name;
+	value[0] = _name;
+	
+	Json::Value &pos = value[1];
+	pos[0] = _pos.X;
+	pos[1] = _pos.Y;
+	pos[2] = _pos.Z;
+
+	Json::Value &size = value[2];
+	size[0] = _size.Width;
+	size[1] = _size.Height;
+
+	value[3] = _description;
+
+	Json::Value &image = value[4];
+	for(std::list<std::string>::const_iterator i = _image.begin(); i != _image.end(); i++)
+	{
+		image.append(*i);
+	}
+
+	Json::Value &audio = value[5];
+	for(std::list<std::string>::const_iterator i = _audio.begin(); i != _audio.end(); i++)
+	{
+		audio.append(*i);
+	}
+
+	Json::Value &video = value[6];
+	for(std::list<std::string>::const_iterator i = _video.begin(); i != _video.end(); i++)
+	{
+		video.append(*i);
+	}
 
 	return writer.write(value);
 }
--- server/source/3DNavigator/Hotspot.h
+++ server/source/3DNavigator/Hotspot.h
@@ -4,6 +4,7 @@
 #include <irrlicht.h>
 #include <json/json.h>
 #include <string>
+#include <list>
 
 class Hotspot
 {
@@ -26,11 +27,23 @@ public:
 	inline void setNode(irr::scene::ISceneNode *node){_node = node;}
 	inline irr::scene::ISceneNode *getNode() const{return _node;}
 
+	inline std::string getDescription() const{return _description;}
+	inline void setDescription(const std::string &description){_description = description;}
+
+	inline void addImage(const std::string &path){_image.push_back(path);}
+	inline void removeImage(const std::string &path){_image.remove(path);}
+	inline void addAudio(const std::string &path){_audio.push_back(path);}
+	inline void removeAudio(const std::string &path){_audio.remove(path);}
+	inline void addVideo(const std::string &path){_video.push_back(path);}
+	inline void removeVideo(const std::string &path){_video.remove(path);}
+
 protected:
 	irr::core::vector3df _pos;
 	irr::core::dimension2d<irr::f32> _size;
 	std::string _name;
 	irr::scene::ISceneNode *_node;
+	std::string _description;
+	std::list<std::string> _image, _audio, _video;
 };
 
 #endif
