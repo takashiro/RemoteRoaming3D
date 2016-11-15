0af2ab8a0aa0d22f3e10df11286175807bdb5c2b
Takashiro <takashiro@qq.com>
2016-11-16 00:42:00 +0800

    Only openGL is accepted

--- server/source/3DNavigator/main.cpp
+++ server/source/3DNavigator/main.cpp
@@ -34,40 +34,10 @@ easy, we use a pragma comment lib:
 /* Ok, lets start. We use the main() method as start, not the WinMain(). */
 
 int main()
-{
-	/*
-	We create an IrrlichtDevice with
-	createDevice(). The difference now is that we ask the user to select
-	which video driver to use. The Software device might be
-	too slow to draw a huge Quake 3 map, but just for the fun of it, we make
-	this decision possible, too.
-	*/
-
-	// ask user for driver
-	irr::video::E_DRIVER_TYPE driverType;
-
-	std::cout << "Please select the driver you want for this example:\n"\
-		" (a) OpenGL 1.5\n (b) Direct3D 9.0c\n (c) Direct3D 8.1\n"\
-		" (d) Burning's Software Renderer\n (e) Software Renderer\n"\
-		" (f) NullDevice\n (otherKey) exit\n$ ";
-
-	char i;
-	std::cin >> i;
-
-	switch(i)
-	{
-		case 'a': driverType = irr::video::EDT_OPENGL;break;
-		case 'b': driverType = irr::video::EDT_DIRECT3D9;break;
-		case 'c': driverType = irr::video::EDT_DIRECT3D8;break;
-		case 'd': driverType = irr::video::EDT_BURNINGSVIDEO;break;
-		case 'e': driverType = irr::video::EDT_SOFTWARE;break;
-		case 'f': driverType = irr::video::EDT_NULL;break;
-		default: return 1;
-	}
-	
+{	
 	//create a server
 	Server server;
-	server.setDriverType(driverType);
+	server.setDriverType(irr::video::EDT_OPENGL);
 	server.loadSceneMap("scenemap.json");
 
 	ControlPanel cpanel(std::cin, std::cout);
