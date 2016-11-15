59e2954c87e5df0edf4259c818b56c6464a17851
Takashiro <takashiro@qq.com>
2016-11-15 23:27:32 +0800

    Fix base64_encode declaration

--- server/source/3DNavigator/util.h
+++ server/source/3DNavigator/util.h
@@ -8,7 +8,7 @@ void copy_string(std::wstring &wstr, const std::string &str);
 
 void urldecode(std::string &str);
 
-std::string base64_encode(const char *data, unsigned long long length);
+std::string base64_encode(const unsigned char *data, unsigned long long length);
 std::string base64_encode(const std::string &str);
 
 std::string sha1(const char *data, unsigned long long length);
