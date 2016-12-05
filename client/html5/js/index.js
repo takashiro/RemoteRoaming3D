838401c13fb5c5f9397f67af06367f364d36c829
Takashiro <takashiro@qq.com>
2016-12-05 14:13:41 +0800

    Add connection dialog for HTML5 client

--- client/html5/js/index.js
+++ client/html5/js/index.js
@@ -22,6 +22,136 @@ var Camera = {
 	y: 0
 };
 
+class Server {
+
+	constructor(url){
+		this.setUrl(url);
+		this.socket = null;
+		this.onopen = [];
+		this.onclose = [];
+		this.onmessage = null;
+	}
+
+	setUrl(url){
+		if(url){
+			var absolute_path = /^\w+:\/\/.+/i;
+			if(absolute_path.test(url)){
+				this.url = url;
+			}else{
+				this.url = 'ws://' + url;
+			}
+		}else{
+			this.url = '';
+		}
+	}
+
+	connect(url){
+		this.setUrl(url);
+		if(!this.url){
+			return false;
+		}
+
+		var server = this;
+		this.socket = new WebSocket(this.url);
+		this.socket.onopen = function(){
+			for(var i = 0; i < server.onopen.length; i++){
+				server.onopen[i]();
+			}
+		};
+		this.socket.onmessage = function (e) {
+			var packet = JSON.parse(e.data);
+			var command = packet[0];
+			var args = packet[1];
+			if (server.onmessage && server.onmessage[command]) {
+				server.onmessage[command](args);
+			}
+		};
+		this.socket.onclose = function(e) {
+			for(var i = 0; i < server.onclose.length; i++){
+				server.onclose[i](e);
+			}
+			server.socket = null;
+		};
+
+		return true;
+	}
+
+	disconnect(){
+		if(this.socket){
+			this.socket.close();
+			this.socket = null;
+		}
+	}
+
+	get isConnected(){
+		return this.socket != null;
+	}
+
+	request(command, args = null){
+		if(this.socket){
+			var packet = [command, args];
+			this.socket.send(JSON.stringify(packet));
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	onOpen(callback){
+		this.onopen.push(callback);
+	}
+
+	onClose(callback){
+		this.onclose.push(callback);
+	}
+
+	onMessage(command, callback){
+		this.onmessage[command] = callback;
+	}
+
+};
+
+class Dialog {
+
+	constructor(){
+		this.title = '';
+		this.titleTag = 'h4';
+		this.content = null;
+		this.elapse = 3;
+	}
+
+	exec(){
+		var title = $('<' + this.titleTag + '></' + this.titleTag + '>');
+		title.html(this.title);
+
+		var dialog = $('<div></div>');
+		dialog.addClass('dialog');
+		dialog.append(title);
+		if(this.content){
+			dialog.append(this.content);
+		}
+
+		dialog.hide();
+		$('#board').append(dialog);
+
+		dialog.css('top', '+=10px');
+		dialog.animate({
+			'top' : '-=10px',
+			'opacity' : 1
+		}, 500);
+
+		if(this.elapse > 0){
+			setTimeout(function(){
+				dialog.fadeOut(function(){
+					dialog.remove();
+				});
+			}, this.elapse * 1000);
+		}
+	}
+
+}
+
+
 var Callback = new Array(Command.NumOfCommands);
 
 Callback[Command.UpdateVideoFrame] = function(arg) {
@@ -51,81 +181,60 @@ Callback[Command.ListMap] = function(args) {
 	}
 };
 
-var Server = {
-	url : 'ws://127.0.0.1:52600/',
-	socket : null,
+var server = new Server();
+server.onmessage = Callback;
 
-	connect : function () {
-		this.socket = new WebSocket(this.url);
-		this.socket.onmessage = function (e) {
-			var packet = JSON.parse(e.data);
-			var command = packet[0];
-			var args = packet[1];
-			if (Callback[command]) {
-				Callback[command](args);
-			}
-		};
-	},
+server.onOpen(function(){
+	$('#screen').html('连接成功');
+	Server.request(Command.ListMap);
 
-	request : function (command, args = null) {
-		var packet = [command, args];
-		this.socket.send(JSON.stringify(packet));
+	var screen = $('#screen');
+
+	screen.mousedown(function(e){
+		e.preventDefault();
+		Camera.attached = true;
+		Camera.x = e.pageX;
+		Camera.y = e.pageY;
+	});
+
+	screen.mouseup(function(){
+		Camera.attached = false;
+	});
+
+	screen.mousemove(function(e){
+		if (Camera.attached) {
+			var deltaX = e.pageX - Camera.x;
+			var deltaY = e.pageY - Camera.y;
+			Camera.x = e.pageX;
+			Camera.y = e.pageY;
+			Server.request(Command.RotateCamera, [deltaX * 100, deltaY * 100]);
+		}
+	});
+
+	screen.mousewheel(function(e){
+		e.preventDefault();
+		Server.request(Command.ScaleCamera, [-e.deltaY * 2000]);
+	});
+});
+
+server.onClose(function(e){
+	switch(e.code){
+	case 1000:
+		$('#screen').html('服务器已关闭');
+		break;
+	case 1006:
+		$('#screen').html('无法连接到服务器');
+		break;
+	default:
+		$('#screen').html('未知错误');
 	}
-};
+});
 
 $(function(){
+	//Forbid selection
 	var body = $('body')[0];
 	body.onselectstart = function(){
 		return false;
-	};
-
-	$('#screen').html('正在连接服务器……');
-	Server.connect();
-
-	Server.socket.onopen = function(){
-		$('#screen').html('连接成功');
-		Server.request(Command.ListMap);
-
-		var screen = $('#screen');
-
-		screen.mousedown(function(e){
-			e.preventDefault();
-			Camera.attached = true;
-			Camera.x = e.pageX;
-			Camera.y = e.pageY;
-		});
-
-		screen.mouseup(function(){
-			Camera.attached = false;
-		});
-
-		screen.mousemove(function(e){
-			if (Camera.attached) {
-				var deltaX = e.pageX - Camera.x;
-				var deltaY = e.pageY - Camera.y;
-				Camera.x = e.pageX;
-				Camera.y = e.pageY;
-				Server.request(Command.RotateCamera, [deltaX * 100, deltaY * 100]);
-			}
-		});
-
-		screen.mousewheel(function(e){
-			e.preventDefault();
-			Server.request(Command.ScaleCamera, [-e.deltaY * 2000]);
-		});
-	};
-
-	Server.socket.onclose = function(e){
-		switch(e.code){
-		case 1000:
-			$('#screen').html('服务器已关闭');
-			break;
-		case 1006:
-			$('#screen').html('无法连接到服务器');
-			break;
-		default:
-			$('#screen').html('未知错误');
-		}
 	};
 
 	$('#scene-map').on('click', 'button', function(e){
@@ -135,7 +244,8 @@ $(function(){
 		var width = screen.width();
 		var height = screen.height();
 
-		screen.html('加载中……');
-		Server.request(Command.CreateDevice, [width, height, id]);
+		if(server.isConnected()){
+			server.request(Command.CreateDevice, [width, height, id]);
+		}
 	});
 });
